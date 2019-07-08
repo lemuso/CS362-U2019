53ae52e202880f7edfc79b359934ed8d890887d4
Oscar Lemus <lemuso@flip2.engr.oregonstate.edu>
2019-07-07 21:14:13 -0700

    Added all requirements for assignment 2

--- projects/lemuso/dominion/dominion_helpers.h
+++ projects/lemuso/dominion/dominion_helpers.h
@@ -11,5 +11,10 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
+void playBaron(int currentPlayer, int choice1, struct gameState *state); 
+void playMinion(int choice1, int choice2, int currentPlayer, int handPos, struct gameState *state); 
+int playAmbassador(int choice1, int choice2, int currentPlayer, int handPos, struct gameState *state); 
+int playTribute(int* tributeRevealedCards, int currentPlayer, int nextPlayer, struct gameState *state); 
+int playMine(int choice1, int choice2, int handPos, int currentPlayer, struct gameState *state); 
 
 #endif
